739dac59842e3813b48d6398b352e0fee5dea33d
Specter <106793765+IamSpectera@users.noreply.github.com>
2023-12-29 22:57:40 -0500

    Starting

--- src/main/kotlin/com/specter/chunkchestkotlin/ChunkChest.kt
+++ src/main/kotlin/com/specter/chunkchestkotlin/ChunkChest.kt
@@ -6,7 +6,6 @@ import org.bukkit.plugin.java.JavaPlugin
 class ChunkChest : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
-
         server.pluginManager.registerEvents(ChunkChestListener(this), this)
         getCommand("chunkchest")!!.setExecutor(ChunkChestCommand(this))
 
